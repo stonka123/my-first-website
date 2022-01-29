20e41bb5690bb8910273523af0547c10825e35d8
stonka123 <stonk123@github.com>
2022-01-29 21:35:17 +0100

    add team aboutus offers

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const burgerBtn = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.mobile');
+const navMobileItem = document.querySelectorAll('.mobile__item');
 
 const showNav = () => {
 	burgerBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('mobile-active');
+	navMobileItem.forEach((item) => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('mobile-active');
+		});
+	});
 };
-
 burgerBtn.addEventListener('click', showNav);
---
+++ js/slick.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+// 	$('.section__team').slick({
+// 		slidesToShow: 1,
+// 		slidesToScroll: 1,
+// 		autoplay: true,
+// 		arrows: false,
+// 		autoplaySpeed: 2000,
+// 		mobileFirst: true,
+// 	});
+// });
