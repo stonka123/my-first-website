b58b1ed8f6c4def59ae13bb585afb764f82b41bf
Tomasz <stonka123@gmail.com>
2022-02-02 18:41:07 +0100

    finish first website

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const burgerBtn = document.querySelector('.hamburger')
+const burgerColor = document.querySelector('.hamburger-inner')
 const navMobile = document.querySelector('.mobile')
 const navMobileItem = document.querySelectorAll('.mobile__item')
 const footYear = document.querySelector('.footer__year')
+const main = document.querySelector('.main')
 
 const showNav = () => {
 	burgerBtn.classList.toggle('is-active')
@@ -11,6 +13,7 @@ const showNav = () => {
 		item.addEventListener('click', () => {
 			navMobile.classList.remove('mobile-active')
 			document.body.classList.remove('sticky-body')
+			burgerBtn.classList.remove('is-active')
 		})
 	})
 }
@@ -19,5 +22,18 @@ const checkYear = () => {
 	footYear.innerHTML = year
 }
 checkYear()
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	if (main.classList.contains('main') && main.offsetTop <= currentSection + 80) {
+		burgerColor.classList.add('green-bars-color')
+	} else {
+		burgerColor.classList.remove('green-bars-color')
+	}
+
+	
+}
+
 
 burgerBtn.addEventListener('click', showNav)
+window.addEventListener('scroll', handleObserver)
